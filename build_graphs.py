d409c749adfaed81447a898772deb881c1c8cc10
AT017314 <Enrique.Molina@idiada.com>
2022-05-29 00:47:30 +0200

    point 1 *metrics* done

--- build_graphs.py
+++ build_graphs.py
@@ -1,7 +1,9 @@
 import json
+import statistics
 
 import matplotlib.pyplot as plt
 import networkx as nx
+from numpy.core.defchararray import upper
 
 from definitions import zones
 
@@ -22,7 +24,14 @@ if __name__ == "__main__":
                         g.add_edge(k['hotspot_id'], m['address'])
         plt.gca().invert_xaxis()
         plt.gca().invert_yaxis()
-        # wm = plt.get_current_fig_manager()
-        # wm.window.state('zoomed')
-        nx.draw_networkx(g, pos=nx.get_node_attributes(g, 'pos'), node_size=10, with_labels=False, width=0.3)
-        plt.show()
+        # nx.draw_networkx(g, pos=nx.get_node_attributes(g, 'pos'), node_size=10, with_labels=False, width=0.3)
+        # plt.show()
+        print('**************')
+        print(upper(i['name']))
+        print('\tNumber of hotstops: ' + str(nx.number_of_nodes(g)))
+        print('\tNumber of witnessing: ' + str(nx.number_of_edges(g)))
+        print('\tMax in degree: ' + str(max(list(len(g.in_edges(i)) for i in nx.nodes(g)))))
+        print('\tMax out degree: ' + str(max(list(len(g.out_edges(i)) for i in nx.nodes(g)))))
+        print('\tAvg in degree: ' + str(statistics.mean(list(len(g.in_edges(i)) for i in nx.nodes(g)))))
+        print('\tAvg out degree: ' + str(statistics.mean(list(len(g.out_edges(i)) for i in nx.nodes(g)))))
+        print('\tAvg clustering coefficient: ' + str(nx.average_clustering(g)))
--- definitions.py
+++ definitions.py
@@ -3,20 +3,20 @@ zones = [
     #     'name': 'catalunya',
     #     'url': 'https://api.helium.io/v1/hotspots/location/box?swlat=40.537004&swlon=0.435048&nelat=42.504967&nelon=3.400427',
     # },
-    {
-        'name': 'barcelona',
-        'url': 'https://api.helium.io/v1/cities/YmFyY2Vsb25hY2F0YWx1bnlhc3BhaW4/hotspots',
-    },
-    {
-        'name': 'tarragona',
-        'url': 'https://api.helium.io/v1/cities/dGFycmFnb25hY2F0YWx1bnlhc3BhaW4/hotspots',
-    },
-    {
-        'name': 'lleida',
-        'url': 'https://api.helium.io/v1/cities/bGxlaWRhY2F0YWx1bnlhc3BhaW4/hotspots',
-    },
+    # {
+    #     'name': 'barcelona',
+    #     'url': 'https://api.helium.io/v1/cities/YmFyY2Vsb25hY2F0YWx1bnlhc3BhaW4/hotspots',
+    # },
+    # {
+    #     'name': 'tarragona',
+    #     'url': 'https://api.helium.io/v1/cities/dGFycmFnb25hY2F0YWx1bnlhc3BhaW4/hotspots',
+    # },
+    # {
+    #     'name': 'lleida',
+    #     'url': 'https://api.helium.io/v1/cities/bGxlaWRhY2F0YWx1bnlhc3BhaW4/hotspots',
+    # },
     {
         'name': 'girona',
-        'url': 'https://api.helium.io/v1/cities/cGxhdGphIGQnYXJvY2F0YWx1bnlhc3BhaW4/hotspots',
+        'url': 'https://api.helium.io/v1/cities/Z2lyb25hY2F0YWx1bnlhc3BhaW4/hotspots',
     }
 ]
